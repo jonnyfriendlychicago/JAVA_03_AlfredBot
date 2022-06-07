181640f3175bfa769a53ec5854cb8c05a602281b
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-06-06 21:43:38 -0500

    MiViProd done, shall now work on overload

---
+++ AlfredQuotes.java
@@ -0,0 +1,43 @@
+import java.util.Date; 
+
+public class AlfredQuotes {
+
+    public String basicGreeting() {
+        return "Hello, lovely to see you.  How are you?"; 
+    }
+
+    public String guestGreeting(String guestName) {
+        
+        String greetingPart1 = "Hello, "; 
+        String greetingPart2 = ".  Lovely to see you."; 
+        String fullMonty = greetingPart1.concat(guestName).concat(greetingPart2);  
+        return fullMonty; 
+    }
+
+    public String dateAnnouncement() {
+        String greetingPart1 = "Hello.  It is currently "; 
+        Date currentDate = new Date(); 
+        // String currentDateTexty = (string) currentDate;
+        // String fullMonty = greetingPart1.concat(currentDateTexty); 
+        return greetingPart1 + currentDate; 
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+       int AlexisCalled = conversation.indexOf("Alexis"); 
+       int AlfredCalled = conversation.indexOf("Alfred");
+
+        if (AlexisCalled > -1)  {
+            return "I'll do that now, sir. Alexis isn't sophisticated enough for that.";  
+        }
+
+        else if (AlfredCalled > -1) {
+            return "At your service. As you wish, naturally."; 
+        }
+
+        else {
+            return "Right. And with that I shall retire.";  
+        }
+
+    }
+
+}
